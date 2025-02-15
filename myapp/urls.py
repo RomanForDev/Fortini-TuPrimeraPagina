3937b68a3348a8003f420d0cb650900999fb3c56
RomanForDev <romanfortinidev@gmail.com>
2025-02-15 18:28:22 -0300

    correccion de sintaxis

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,6 @@ from myapp.views import inicio, crear_piedra, listar_piedras
 
 urlpatterns= [
     path('', inicio, name= 'inicio'),
-    path('crear-piedra/', crear_piedra, name= 'crear_piedra'),
-    path('listar-piedra/', listar_piedras, name= 'listar_piedras')
+    path('crear-piedra/', crear_piedra, name= "crear_piedra"),
+    path('listar-piedra/', listar_piedras, name= "listar_piedras")
 ]
