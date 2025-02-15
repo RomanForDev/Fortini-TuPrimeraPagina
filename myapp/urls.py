40684d5eac9ade2ecf7ca469b2e73b1eac311216
RomanForDev <romanfortinidev@gmail.com>
2025-02-15 20:01:34 -0300

    no se

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,5 @@ from myapp.views import inicio, crear_piedra, listar_piedras
 urlpatterns= [
     path('', inicio, name= 'inicio'),
     path('crear-piedra/', crear_piedra, name= "crear_piedra"),
-    path('listar-piedra/', listar_piedras, name= "listar_piedras")
+    path('listar-piedra/', listar_piedras, name= "listar_piedras"),
 ]
