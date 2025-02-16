5860992ba24d5593e6b1cd38047ece11e54efc4d
RomanForDev <romanfortinidev@gmail.com>
2025-02-16 12:44:22 -0300

    Sintaxis

--- myapp/forms.py
+++ myapp/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class CrearPiedra(forms.Form):
     clase= forms.CharField(max_length=20),
     material= forms.CharField(max_length=20),
-    descripcion= forms.CharField(required= False, widget= forms.Textarea)
+    descripcion= forms.CharField(required=False, widget=forms.Textarea)
     
--- myapp/models.py
+++ myapp/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class piedra(models.Model):
     clase= models.CharField(max_length=20)
     material= models.CharField(max_length=20)
-    descripcion= models.TextField(null=True, blank= True)
+    descripcion= models.TextField(null=True, blank=True)
